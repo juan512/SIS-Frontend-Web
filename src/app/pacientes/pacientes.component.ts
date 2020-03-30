a1cbf1e52d82ead0f2061a2d8db2a8aa0ec4f3f0
Nanita29 <nardaorihuela@gmail.com>
2020-03-30 01:26:59 -0300

    Mostrar datos de pacientes

--- src/app/pacientes/pacientes.component.ts
+++ src/app/pacientes/pacientes.component.ts
@@ -1,24 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuariosService } from '../services/usuarios.service';
-import { HttpClientModule } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
-import { User } from '../interfaces/user';
+import {User} from '../interfaces/user'
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: 'app-pacientes',
+  templateUrl: './pacientes.component.html',
+  styleUrls: ['./pacientes.component.css']
 })
-export class HomeComponent implements OnInit {
-  /* API_ENDPOINT= 'http://181.188.163.198/SIS-Backend/app/Http/Controllers/AuthController'
+export class PacientesComponent implements OnInit {
+
+  API_ENDPOINT= 'http://181.188.163.198/SIS-Backend/app/Http/Controllers/AuthController'
   users: User[];
   constructor(private ususuarioService: UsuariosService, private httpClient: HttpClient) {
       httpClient.get(this.API_ENDPOINT + '/getUser').subscribe((data:User[]) => {
         this.users=data;
     });
-      } */
+      }
 
-  ngOnInit() {
-  }
-
+      ngOnInit() {
+      }
 }
